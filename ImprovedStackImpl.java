4a8879b038f929c2c9d9f18d93d83cd787884889
ahla_abir arbid <ahla_abir arbid>
2016-01-08 21:33:55 +0000

    outline last exercise

---
+++ ImprovedStackImpl.java
@@ -0,0 +1,34 @@
+/**
+ * An implementation of a stack with additional methods. 
+ 
+ * @author Ahla Arbid
+ */
+
+ public class ImprovedStackImpl implements ImprovedStack{
+	
+		/**
+	 * Returns a copy of this stack with the items reversed, the top
+	 * elements on the original stack is at the bottom of the new
+	 * stack and viceversa.
+	 * 
+	 * @return a copy of this stack with the items reversed. 
+	 */
+	public ImprovedStack reverse(){
+	
+	}
+	
+	
+	/**
+	 * Removes the given object from the stack if it is
+	 * there. Multiple instances of the object are all removed.
+	 *
+	 * Classes implementing this method must use method .equals() to
+	 * check whether the item is in the stack or not.
+	 * 
+	 * @param object the object to remove
+	 */
+	public void remove(Object object){
+	
+	}
+
+ }
